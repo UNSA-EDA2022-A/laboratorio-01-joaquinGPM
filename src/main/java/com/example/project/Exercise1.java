f2e0cedc01896e114524f5a9b68cfc7af8af3085
joaquinGPM <91225726+joaquinGPM@users.noreply.github.com>
2022-05-08 22:01:34 -0500

    Update Exercise1.java

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,7 +16,29 @@ public class Exercise1 {
 
 	public String convertirBaseGabriel(int a) {
 
-		// TO DO
-		return "El numero proporcionado no esta en base Gabriel.";
+		int num_decimal=0, digito, contador = 1, n;
+	        int es_gabriel=1, min_significativo=0;
+
+	        while (a>0)
+	        {
+	            digito = a%10;
+	        
+	        if(digito !=0 && digito !=1 && (digito==2 && min_significativo==0)){
+	            es_gabriel=0;
+
+	        }
+	        if(digito==1){
+	            min_significativo = 1;
+	        }
+	        int tap= (int) Math.pow(2,contador);
+	        num_decimal=num_decimal+digito*(tap-1);
+	        contador=contador+1;
+	        a= a/10;
+			
+		}
+	    if(es_gabriel ==1)
+	        return ""+ num_decimal;
+	    else
+	    return "El numero proporcionado no esta en base Gabriel.";
 	}
 }
